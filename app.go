303fcb29528b384508c17885119f2bb4bc1d66d2
Kurt Obando <kurtobando.developer@gmail.com>
2024-06-02 22:45:29 +0800

    initial commit

--- app.go
+++ app.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(getName("john doe"))
+	fmt.Println(getAge(21))
+}
+func getName(name string) string {
+	return name
+}
+func getAge(age int) int {
+	return age
 }
