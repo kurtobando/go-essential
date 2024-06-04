fed7c953de03cd79391e6479562f1366b76d8664
Kurt Obando <kurtobando.developer@gmail.com>
2024-06-04 22:17:19 +0800

    add method to struct

--- app.go
+++ app.go
@@ -2,10 +2,18 @@ package main
 
 import "fmt"
 
+// create type user
 type user struct {
 	name     string
 	age      int
 	location string
+}
+
+// attach a method to user struct
+func (u user) displayUser() {
+	fmt.Println(u.name)
+	fmt.Println(u.age)
+	fmt.Println(u.location)
 }
 
 func main() {
@@ -14,8 +22,5 @@ func main() {
 		age:      21,
 		location: "New York",
 	}
-
-	fmt.Println(appUser.name)
-	fmt.Println(appUser.age)
-	fmt.Println(appUser.location)
+	appUser.displayUser()
 }
