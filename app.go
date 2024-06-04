2505be6ca3e767eaf727aad63ec281d52a014f12
Kurt Obando <kurtobando.developer@gmail.com>
2024-06-05 00:02:38 +0800

    rename struct to capital letter

--- app.go
+++ app.go
@@ -5,29 +5,28 @@ import (
 	"fmt"
 )
 
-// create type user
-type user struct {
+type User struct {
 	name     string
 	age      int
 	location string
 }
 
-// attach a method to user struct
-func (u *user) displayUser() {
+// attach a method to User struct
+func (u *User) displayUser() {
 	fmt.Printf("%s %d %s \n", u.name, u.age, u.location)
 }
 
 // attach a method that will mutate the params
-func (u *user) setName(name string) {
+func (u *User) setName(name string) {
 	u.name = name
 }
 
 // convention when creating a struct
-func newUser(name string, age int, location string) (*user, error) {
+func newUser(name string, age int, location string) (*User, error) {
 	if name == "" || age == 0 || location == "" {
 		return nil, errors.New("invalid arguments")
 	}
-	return &user{
+	return &User{
 		name:     name,
 		age:      age,
 		location: location,
@@ -35,16 +34,16 @@ func newUser(name string, age int, location string) (*user, error) {
 }
 
 func main() {
-	_user, err := newUser("John Doe", 21, "New York")
+	user, err := newUser("John Doe", 21, "New York")
 	if err != nil {
 		fmt.Println(err)
 	}
-	_user.displayUser()
-	_user.setName("Mary Doe")
-	_user.displayUser()
+	user.displayUser()
+	user.setName("Mary Doe")
+	user.displayUser()
 
 	// return memory address
-	fmt.Println(&_user)
+	fmt.Println(&user)
 	// return the value of the memory address
-	fmt.Println(*_user)
+	fmt.Println(*user)
 }
