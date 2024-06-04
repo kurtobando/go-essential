668260c59c2e888b7c09bd1ed545691e92f9f321
Kurt Obando <kurtobando.developer@gmail.com>
2024-06-04 21:40:33 +0800

    initial commit

--- app.go
+++ app.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type user struct {
+	name     string
+	age      int
+	location string
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var appUser user = user{
+		name:     "John Doe",
+		age:      21,
+		location: "New York",
+	}
+
+	fmt.Println(appUser.name)
+	fmt.Println(appUser.age)
+	fmt.Println(appUser.location)
 }
