c4ece174c49bd2246acc6ea7c8914a059279dc20
Kurt Obando <kurtobando.developer@gmail.com>
2024-06-11 21:54:26 +0800

    pass function as param

--- app.go
+++ app.go
@@ -2,13 +2,44 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(getName("john doe"))
-	fmt.Println(getAge(21))
-}
 func getName(name string) string {
 	return name
 }
+
 func getAge(age int) int {
 	return age
+}
+
+func double(i int) int {
+	return i * 2
+}
+
+func triple(i int) int {
+	return i * 3
+}
+
+// params: array, func()
+func transformNumber(i []int, transform func(int) int) []int {
+	var result []int
+
+	for _, v := range i {
+		result = append(result, transform(v))
+	}
+
+	return result
+}
+
+func main() {
+	fmt.Println(getName("john doe"))
+	fmt.Println(getAge(21))
+
+	var numbers = []int{1, 2, 3}
+
+	// double
+	d := transformNumber(numbers, double)
+	fmt.Println(d)
+
+	// triple
+	t := transformNumber(numbers, triple)
+	fmt.Println(t)
 }
