a2139c57131d8f4ea12dfead6ed3b3c77f3c4bf8
Kurt Obando <kurtobando.developer@gmail.com>
2024-06-12 10:45:12 +0800

    initial commit

--- app.go
+++ app.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	price := []float64{9.99, 2.99, 4.29}
+	taxRates := []float64{0.1, 0.15, 0.05}
+	taxRatesWithPrice := make(map[float64][]float64)
+
+	for _, trv := range taxRates {
+		priceWithTax := make([]float64, len(price))
+		for pi, pv := range price {
+			//priceWithTax = append(priceWithTax, pv*trv)
+			priceWithTax[pi] = pv * trv
+		}
+		taxRatesWithPrice[trv] = priceWithTax
+	}
+
+	fmt.Println(taxRatesWithPrice)
+
 }
